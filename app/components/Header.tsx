85f5cd783329bc653659fb9ab79aba2eefe730c1
youkwhd <lolywk@tutanota.com>
2024-09-25 18:17:50 +0700

    feat: complete header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,6 @@
+import { HiOutlineShoppingCart, HiOutlineSearch } from "react-icons/hi";
+import { Link } from "@remix-run/react";
+
 const menu: any[] = [
     {
         name: "Stock",
@@ -15,19 +18,23 @@ const menu: any[] = [
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-between">
-            <div className="flex">
-                <div>
-                    <span>MyMart</span>
+        <header className="flex items-center justify-between mx-2 py-5 border-neutral-800 border-b">
+            <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center mr-4">
+                    <HiOutlineShoppingCart className="mr-4" />
+                    <span className="text-lg">Mart</span>
                 </div>
-                <div>
-                    <input type="text" placeholder="Search" />
-                </div>
+                <form className="ml-4 flex items-center justify-center rounded bg-neutral-900 p-1" onSubmit={(e) => e.preventDefault()}>
+                        <HiOutlineSearch className="m-2" />
+                        <input className="mx-2 py-1 bg-transparent outline-none" type="text" placeholder="Quick Search" />
+                </form>
             </div>
             <div>
                 <ul className="flex flex-row">
-                    {menu.map((m: any) => (
-                        <li className="mr-2">{m.name}</li>
+                    {menu.map((m: any, i: number) => (
+                        <li className="ml-8" key={i}>
+                            <Link to={m.link}>{m.name}</Link>
+                        </li>
                     ))}
                 </ul>
             </div>
--- app/root.tsx
+++ app/root.tsx
@@ -17,8 +17,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
       </head>
       <body>
-        <Header />
-        {children}
+        <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 md:px-8">
+            <Header />
+            {children}
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
