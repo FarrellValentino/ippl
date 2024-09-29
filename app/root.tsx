c2c34d09b1c1b31603b982dded8c4761245b8b06
youkwhd <lolywk@tutanota.com>
2024-09-29 16:52:58 +0700

    chore: spaces

--- app/root.tsx
+++ app/root.tsx
@@ -9,25 +9,25 @@ import Header from "~/components/Header";
 import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-      </head>
-      <body>
-        <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 md:px-8">
-            <Header />
-            {children}
-        </div>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta />
+            </head>
+            <body>
+                <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 md:px-8">
+                    <Header />
+                    {children}
+                </div>
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return <Outlet />;
+    return <Outlet />;
 }
