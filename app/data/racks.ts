f4dc30c6b15a11230374f229efaff364071b0617
youkwhd <lolywk@tutanota.com>
2024-09-29 20:20:06 +0700

    feat: add racks, visual receipt

---
+++ app/data/racks.ts
@@ -0,0 +1,47 @@
+export type RackItem = {
+    name: string,
+    color: string,
+};
+
+export const racks: RackItem[] = [
+    {
+        name: "Snacks",
+        color: "#97FA9A",
+    },
+    {
+        name: "Chocolates",
+        color: "#79B8F4",
+    },
+    {
+        name: "Sandwich",
+        color: "#FCC5D9",
+    },
+    {
+        name: "Cocktails",
+        color: "#D8BBFF",
+    },
+    {
+        name: "Soda",
+        color: "#FFD6A5",
+    },
+    {
+        name: "Noodles",
+        color: "#FDFFB6",
+    },
+    {
+        name: "Apples",
+        color: "#CAFFBF",
+    },
+    {
+        name: "Juice",
+        color: "#FFADAD",
+    },
+    {
+        name: "Cereal",
+        color: "#9BF6FF",
+    },
+    {
+        name: "Rice",
+        color: "#D1B2A1",
+    }
+];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import Rack from "~/components/Rack";
+import { type RackItem, racks } from "~/data/racks";
 
 export const meta: MetaFunction = () => {
     return [
@@ -9,7 +11,16 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
     return (
-        <>
-        </>
+        <div className="flex mt-6">
+            <div className="w-9/12 h-80 overflow-y-scroll flex flex-wrap content-start gap-2">
+                {racks.map((item: RackItem, i: number) => (
+                    <Rack key={`Rack @${i}`} item={item} />
+                ))}
+            </div>
+            <div className="w-3/12 bg-neutral-900">
+                <p className="text-center my-2.5">Receipt</p>
+                <hr className="border-dashed mx-4" />
+            </div>
+        </div>
     );
 }
