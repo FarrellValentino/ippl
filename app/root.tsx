72bba82de6fed8f59d3ee610ca0e118505c13de1
youkwhd <lolywk@tutanota.com>
2024-11-01 19:08:08 +0700

    chore: add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,7 @@ export default ({ children }: { children: React.ReactNode }) => {
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
+                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
                 <Links />
             </head>
             <body>
